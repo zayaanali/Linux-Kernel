fdfdd4b8cce9f3287c6add1d553d2c229363977b
Zayaan Ali <zaali2@illinois.edu>
2023-10-13 17:00:00 -0500

    add pic initialization

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -4,6 +4,7 @@
 
 #include "i8259.h"
 #include "lib.h"
+#include "spinlock.h"
 
 /* Interrupt masks to determine which interrupts are enabled and disabled */
 uint8_t master_mask; /* IRQs 0-7  */
@@ -11,6 +12,25 @@ uint8_t slave_mask;  /* IRQs 8-15 */
 
 /* Initialize the 8259 PIC */
 void i8259_init(void) {
+    /* Don't need spinlock/or mask interrupt bc we only initialize on start */
+
+    /* Set to cascade mode */
+    outb(ICW1, MASTER_8259_PORT);
+    outb(ICW1, SLAVE_8259_PORT);
+
+    /* Set interrupt vector offset (where to start interrupts from interrupt table) */
+    outb(ICW2_MASTER, MASTER_8259_PORT);
+    outb(ICW2_SLAVE, SLAVE_8259_PORT);
+    
+    /* Set up cascading */
+    outb(ICW3_MASTER, MASTER_8259_PORT); // Set slave on IRQ2
+    outb(ICW3_SLAVE, SLAVE_8259_PORT); // Set slave on IRQ2
+
+    /* Set to 8086 mode */
+    outb(ICW4, MASTER_8259_PORT);
+    outb(ICW4, SLAVE_8259_PORT);
+
+
 }
 
 /* Enable (unmask) the specified IRQ */
