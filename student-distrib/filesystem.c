21bb2bc56f95e7ff00b220150382becf99ae9de5
Lanz <mlanz2@illinois.edu>
2023-10-17 16:59:54 -0500

    added 2 filesystem functions, modifed filesystem struct

--- student-distrib/filesystem.c
+++ student-distrib/filesystem.c
@@ -1,15 +1,48 @@
 #include "filesystem.h"
 #include "types.h"
+#include "lib.h"
+
+
 
 int32_t read_dentry_by_name (const uint8_t* fname, dentry_t* dentry){
-    return 0; 
+
+    int i;     // loop variable
+    int fname_length = strlen(fname); 
+
+    // parameter check. function only works if fname is 32 bytes
+    if(fname_length!=32){
+        return -1; 
+    }
+
+    // loop through directory entries until match is found
+    for(i=0; i<boot_block.num_dir_entries; i++){
+        if(strncmp(boot_block.dir_entries[i].file_name,fname,32)==0){
+            strcpy(dentry->file_name, boot_block.dir_entries[i].file_name);
+            dentry->file_type = boot_block.dir_entries[i].file_type;
+            dentry->inode_id = boot_block.dir_entries[i].inode_id;
+            return 0; 
+        }
+    }
+
+    // directory entry not found
+    return -1; 
 }
 
 
 int32_t read_dentry_by_index (uint32_t index, dentry_t* dentry){
+
+    // check validity of index
+    if(index > (boot_block.num_dir_entries-1)){
+        return -1; 
+    }
+
+    strcpy(dentry->file_name, boot_block.dir_entries[index].file_name);
+    dentry->file_type = boot_block.dir_entries[index].file_type;
+    dentry->inode_id = boot_block.dir_entries[index].inode_id;
     return 0; 
 }
 
 int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length){
     return 0; 
 }
+
--- student-distrib/filesystem.h
+++ student-distrib/filesystem.h
@@ -4,33 +4,9 @@
 #include "types.h"
 
 #define BLOCK_SIZE 4096     
+#define MAX_NUM_FILES 62
+#define MAX_NUM_DATA_BLOCKS_PER_FILE 1023
 
-extern int32_t read_dentry_by_name (const uint8_t* fname, dentry_t* dentry);
-extern int32_t read_dentry_by_index (uint32_t index, dentry_t* dentry);
-extern int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length);
-
-/* directory entry struct */
-typedef union dentry_t {
-    
-    struct {
-        struct name file_name; 
-        uint32_t file_type; 
-        uint32_t inode_id;
-        struct reserved_24 reserved; 
-    };
-} dentry_t;
-
-/* boot block struct */
-typedef union boot_block_t{
-
-    struct {
-        uint32_t num_dir_entries; 
-        uint32_t num_inodes; 
-        uint32_t num_dbs;               // number of data blocks
-        struct reserved_52 reserved; 
-        dentry_t dir_entries[63];
-    };
-} boot_block_t; 
 
 /* inode struct */
 typedef union inode_t {
@@ -41,20 +17,42 @@ typedef union inode_t {
     };
 } inode_t; 
 
-/* struct for 32B name */
-struct name{
-    char chars[32];
-};
-
-/* struct for 24B reserved */
-struct reserved_24{
-    char data[32];
-};
 
 
-/* struct for 52B reserved */
-struct reserved_52{
-    char data[52];
-};
+/* directory entry struct */
+typedef struct dentry_t {
+    
+    uint8_t file_name[32]; 
+    uint32_t file_type; 
+    uint32_t inode_id;
+    uint8_t reserved[24]; 
+   
+} dentry_t;
+
+/* boot block struct */
+typedef struct boot_block_t{
+
+    uint32_t num_dir_entries; 
+    uint32_t num_inodes; 
+    uint32_t num_dbs;               // number of data blocks
+    uint8_t reserved[52]; 
+    dentry_t dir_entries[63];
+} boot_block_t; 
+
+/* data block struct */
+/* boot block struct */
+typedef struct data_block_t{
+
+    uint8_t data[4096];
+
+} data_block_t; 
+
+extern int32_t read_dentry_by_name (const uint8_t* fname, dentry_t* dentry);
+extern int32_t read_dentry_by_index (uint32_t index, dentry_t* dentry);
+extern int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length);
+
+boot_block_t boot_block; 
+inode_t inodes[MAX_NUM_FILES];
+data_block_t data_blocks[MAX_NUM_FILES*MAX_NUM_DATA_BLOCKS_PER_FILE];
 
 #endif
