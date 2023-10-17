69bccf5e7b52403101a4813142031c9d79fa146c
Ghimire <biping2@illinois.edu>
2023-10-17 18:09:30 -0500

    Read Data Work

--- student-distrib/filesystem.c
+++ student-distrib/filesystem.c
@@ -43,6 +43,47 @@ int32_t read_dentry_by_index (uint32_t index, dentry_t* dentry){
 }
 
 int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length){
-    return 0; 
+    // Check if inode number is valid
+    if (inode_num >= MAX_NUM_FILES) {
+        return -1;
+    }
+
+    // Locate the inode structure
+    inode_t* inode_ptr = &inodes[inode_num];
+
+    // Calculate number of data blocks and block size
+    uint32_t num_blocks = (inode_ptr->length + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+    // Initialize variables for reading data
+    uint32_t read_length = 0;
+    uint32_t block_offset = offset % BLOCK_SIZE;
+    uint32_t block_index = offset / BLOCK_SIZE;
+
+    // Read data from data blocks
+    while (read_length < length && block_index < num_blocks) {
+        // Get block number
+        uint32_t block_no = inode_ptr->dbi[block_index];
+        if (block_no == 0) {
+            return -1; 
+        }
+
+        // Calculate how much data to read from this block
+        uint32_t to_read = length - read_length;
+        if (to_read > BLOCK_SIZE - block_offset) {
+            to_read = BLOCK_SIZE - block_offset;
+        }
+
+        // Read data from block
+        uint8_t* block_ptr = data_blocks[block_no].data;
+        memcpy(buf + read_length, block_ptr + block_offset, to_read);
+
+        // Update variables
+        read_length += to_read;
+        block_index++;
+        block_offset = 0; // Reset offset for next blocks
+    }
+
+    // Return number of bytes read
+    return read_length;
 }
 
